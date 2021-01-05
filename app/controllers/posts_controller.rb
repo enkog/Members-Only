c89a739d7799a3c1550fbe3aa6f2d8273a516fe0
enkog <nkirukaogu@gmail.com>
2021-01-05 14:33:42 +0100

    Add logic to display author's email if user is signed in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
         @post.user = current_user
 
         if @post.save
-        redirect_to root_path
+        redirect_to posts_path
         else
         render :new
         end
